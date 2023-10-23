61f985ddd1b76fda28561a57b113c436d76a65e9
Filmon Ghebremichael <filmo2020gh@gmail.com>
2023-10-23 03:02:27 +0000

    Add  'PostListView' to fetch and serialize posts.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .serializers import PostSerializer
+from rest_framework.views import APIView
+from .models import Post
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class PostListView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
