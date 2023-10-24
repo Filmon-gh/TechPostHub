fdc2cdb80d9f8a3b0207abdcaef6bf3e4d8cd5f8
Filmon Ghebremichael <filmo2020gh@gmail.com>
2023-10-24 09:11:44 +0000

    Add configuration setting for heroku deployment

--- techhabpost/settings.py
+++ techhabpost/settings.py
@@ -55,13 +55,15 @@ REST_AUTH_SERIALIZERS = {
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '<your_app_name>.herokuapp.com']
+DEBUG = True
+
+ALLOWED_HOSTS = ['https://techposthub-acad652d6abc.herokuapp.com']
 
 
 # Application definition
@@ -79,6 +81,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
     'dj_rest_auth.registration',
@@ -139,16 +142,16 @@ WSGI_APPLICATION = 'techhabpost.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DEV' in os.environ:
-     DATABASES = {
+    DATABASES = {
          'default': {
              'ENGINE': 'django.db.backends.sqlite3',
              'NAME': BASE_DIR / 'db.sqlite3',
          }
      }
 else:
-     DATABASES = {
+    DATABASES = {
          'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-     }
+    }
 
 
 # Password validation
